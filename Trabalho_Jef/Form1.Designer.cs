04a972e95c22e3d4c5e142c6e915ae6fdbb57afa
GabrielSheldes <gabriel.arantes9@etec.sp.gov.br>
2025-06-08 21:32:04 -0300

    Produto

--- Trabalho_Jef/Form1.Designer.cs
+++ Trabalho_Jef/Form1.Designer.cs
@@ -32,10 +32,12 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(626, 311);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
